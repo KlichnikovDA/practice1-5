8cc02bef7cb6a4e7b394e2b54da2cf49425d09f7
KlichnikovDA <spacerus99@gmail.com>
2018-04-22 13:57:04 +0500

    Написал программу для заполнения вещественной матрицы и поиска максимального элемента в указанной области.

--- PracticeTask5/Program.cs
+++ PracticeTask5/Program.cs
@@ -10,6 +10,46 @@ namespace PracticeTask5
     {
         static void Main(string[] args)
         {
+            // Генератор случайных чисел для генерации матрицы 
+            Random rnd = new Random();
+            // Размер матрицы
+            ushort N;
+            // Флаг правильности ввода
+            bool ok;
+            do
+            {
+                Console.WriteLine("Введите размер матрицы: ");
+                ok = UInt16.TryParse(Console.ReadLine(), out N) && N > 0;
+                if (!ok)
+                    Console.WriteLine("Требуется ввести натуральное число.");
+            } while (!ok);
+            // Матрица
+            double[,] Matrix = new double[N, N];
+            // Координата середины матрицы
+            ushort Mid = (ushort)(N / 2);
+            // Максимальный элемент
+            double Max = 0;
+
+            // Заполнение матрицы
+            for (ushort i = 0; i < N; i++)
+                for (ushort j = 0; j < N; j++)
+                    Matrix[i, j] = rnd.NextDouble();
+
+            // Проход по заданной области и поиск максимального элемента
+            for (ushort i = 0; i < Mid; i++)
+                for (ushort j = i; j < N - i; j++)
+                    if (Matrix[i, j] > Max)
+                        Max = Matrix[i, j];
+            for (ushort i = Mid; i < N; i++)
+                for (ushort j = (ushort)(N-1-i); j<=i; j++)
+                    if (Matrix[i, j] > Max)
+                        Max = Matrix[i, j];
+
+            // Вывод результатов
+            Console.WriteLine("Наибольший элемент в выделенной области равен {0}.", Max);
+
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
         }
     }
 }
